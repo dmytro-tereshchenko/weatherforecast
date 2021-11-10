ed5c2977fa3c3f9612945bb365735fc2570d120b
Dmytro Tereshchenko <legat4life@gmail.com>
2021-11-10 21:33:24 +0200

    add hourly forecast for page 5-days, add events to search city

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ async function setStartCity() {
     if (response.ok === true) {
         const weather = await response.json();
         const input = document.forms[0].elements[0];
-        input.value = `${weather.name}, ${weather.sys.country}`;
+        input.placeholder = `${weather.name}, ${weather.sys.country}`;
     }
 }
 async function currentWeather() {
@@ -46,41 +46,36 @@ async function currentWeather() {
         sun[2].innerHTML = `${((sunset - sunrise) / 3600000).toFixed(2)} hr`;
     }
 }
-async function hourlyForecastCurrentDayByLocation(lat, lon) {
+async function hourlyForecastByDayByLocation(lat, lon, block, offsetDay) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=80883943e6ce504c831b3aedd952ba89`, {
         method: "GET",
         headers: { "Accept": "application/json" }
     });
     if (response.ok === true) {
         var weather = await response.json();
-        console.log(weather);
         let i = 0;
-        let currentDay = new Array();
-        let endFilter = false;
+        let hourlyForecast = new Array();
         let date;
-        const today = new Date();
-        while (!endFilter && i < weather.list.length) {
+        let targetDay = new Date();
+        targetDay.setDate(targetDay.getDate() + offsetDay);
+        while (i < weather.list.length) {
             date = new Date(weather.list[i].dt * 1000);
             if (date.getHours() == 0)
                 date.setDate(date.getDate() - 1);
-            if (date.getDate() == today.getDate()) {
-                currentDay.push(weather.list[i]);
-                i++;
+            if (date.getDate() == targetDay.getDate()) {
+                hourlyForecast.push(weather.list[i]);
             }
-            else {
-                endFilter = true;
-            }
+            i++;
         }
-        const block = document.querySelector("#today>div:nth-of-type(2) tbody");
         block.innerHTML = '';
         let tr = document.createElement("tr");
         tr.setAttribute("class", "fw-bolder");
         let td = document.createElement("td");
         td.setAttribute("scope", "col");
         td.setAttribute("class", "text-uppercase");
-        td.innerText = "Today";
+        td.innerText = targetDay.getDate() === (new Date()).getDate() ? "Today" : targetDay.toLocaleString('en', { weekday: 'long' });
         tr.append(td);
-        currentDay.forEach(element => {
+        hourlyForecast.forEach(element => {
             td = document.createElement("td");
             td.setAttribute("scope", "col");
             td.innerText = (new Date(element.dt * 1000)).toLocaleString('en-US', { hour: 'numeric' });
@@ -92,7 +87,7 @@ async function hourlyForecastCurrentDayByLocation(lat, lon) {
         td = document.createElement("td");
         tr.append(td);
         let img;
-        currentDay.forEach(element => {
+        hourlyForecast.forEach(element => {
             td = document.createElement("td");
             img = document.createElement("img");
             img.setAttribute("src", `http://openweathermap.org/img/w/${element.weather[0].icon}.png`);
@@ -106,7 +101,7 @@ async function hourlyForecastCurrentDayByLocation(lat, lon) {
         td.setAttribute("class", "fw-bolder");
         td.innerText = "Forecast";
         tr.append(td);
-        currentDay.forEach(element => {
+        hourlyForecast.forEach(element => {
             td = document.createElement("td");
             td.innerText = element.weather[0].description;
             tr.append(td);
@@ -118,7 +113,7 @@ async function hourlyForecastCurrentDayByLocation(lat, lon) {
         td.setAttribute("class", "fw-bolder");
         td.innerHTML = "Temp (&deg;)";
         tr.append(td);
-        currentDay.forEach(element => {
+        hourlyForecast.forEach(element => {
             td = document.createElement("td");
             td.innerHTML = `${Math.round(element.main.temp)}&deg;`;
             tr.append(td);
@@ -130,7 +125,7 @@ async function hourlyForecastCurrentDayByLocation(lat, lon) {
         td.setAttribute("class", "fw-bolder");
         td.innerText = "Real Feel";
         tr.append(td);
-        currentDay.forEach(element => {
+        hourlyForecast.forEach(element => {
             td = document.createElement("td");
             td.innerHTML = `${Math.round(element.main.feels_like)}&deg;`;
             tr.append(td);
@@ -142,7 +137,7 @@ async function hourlyForecastCurrentDayByLocation(lat, lon) {
         td.setAttribute("class", "fw-bolder");
         td.innerText = "Wind (km/h)";
         tr.append(td);
-        currentDay.forEach(element => {
+        hourlyForecast.forEach(element => {
             td = document.createElement("td");
             td.innerText = `${Math.round(element.wind.speed * 3.6)} ${windDeg(element.wind.deg)}`;
             tr.append(td);
@@ -221,7 +216,7 @@ function showToday() {
     document.getElementById("error").style.display = "none";
     document.getElementById("today").style.display = "block";
     currentWeather();
-    hourlyForecastCurrentDayByLocation(lat, lon);
+    hourlyForecastByDayByLocation(lat, lon, document.querySelector("#today>div:nth-of-type(2) tbody"), 0);
     nearbyPlacesByLocation(lat, lon);
 }
 document.querySelector("nav>a:first-of-type").addEventListener("click", function (e) {
@@ -233,6 +228,7 @@ function showForecast() {
     document.getElementById("error").style.display = "none";
     document.getElementById("forecast").style.display = "block";
     daysForecastByLocation(lat, lon);
+    hourlyForecastByDayByLocation(lat, lon, document.querySelector("#forecast>div:last-of-type tbody"), 0);
 }
 document.querySelector("nav>a:last-of-type").addEventListener("click", function (e) {
     e.preventDefault();
@@ -254,6 +250,37 @@ async function daysForecastByLocation(lat, lon) {
             blocks[i].children[2].children[0].setAttribute("src", `http://openweathermap.org/img/w/${weather.daily[i].weather[0].icon}.png`);
             blocks[i].children[3].innerHTML = `${Math.round(i == 0 ? weather.daily[i].temp.night : weather.daily[i].temp.day)}&#8451`;
             blocks[i].children[4].innerText = weather.daily[i].weather[0].description;
+            blocks[i].addEventListener("click", function () {
+                hourlyForecastByDayByLocation(lat, lon, document.querySelector("#forecast>div:last-of-type tbody"), i);
+                blocks.forEach(element => {
+                    element.style.opacity = 1;
+                })
+                this.style.opacity = 0.85;
+            })
         }
     }
 }
+async function searchCity(city) {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.replace(" ", "")}&appid=80883943e6ce504c831b3aedd952ba89`, {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+    if (response.ok === true) {
+        const weather = await response.json();
+        lat = weather.coord.lat;
+        lon = weather.coord.lon;
+    }
+}
+document.querySelector("body>div:first-of-type img").addEventListener("click", async function () {
+    const input = document.forms[0].elements[0];
+    await searchCity(input.value);
+    showToday();
+})
+document.forms[0].elements[0].addEventListener("keydown", async function (e) {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        const input = document.forms[0].elements[0];
+        await searchCity(input.value);
+        showToday();
+    }
+})
